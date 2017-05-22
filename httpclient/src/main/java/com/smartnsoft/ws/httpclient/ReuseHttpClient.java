527da234a094b829c8b3e94b92b75c437d94c6e0
Ludovic Roland <ludovic@smartnsoft.com>
2017-05-22 16:44:22 +0200

    RED-149 #time 15m Add the deprecated httpClient module

--- httpclient/src/main/java/com/smartnsoft/ws/httpclient/ReuseHttpClient.java
+++ httpclient/src/main/java/com/smartnsoft/ws/httpclient/ReuseHttpClient.java
@@ -1,4 +1,4 @@
-package com.smartnsoft.okhttpclientwebservicecaller;
+package com.smartnsoft.ws.httpclient;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
@@ -7,13 +7,13 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * @author Ludovic Roland
- * @since 2016.04.28
+ * An empty interface which states that the underlying client instance should be reused for all HTTP requests, instead of creating a new
+ * one each time.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Inherited
-public @interface ReuseOkHttpClient
+public @interface ReuseHttpClient
 {
 
 }
