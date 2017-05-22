1a3825ef8edef59b7228e116903118dc7b7dbf61
Ludovic Roland <ludovic@smartnsoft.com>
2017-05-22 16:43:54 +0200

    RED-6 #time 15m Update the package name of the okhttp module

---
+++ okhttp/src/main/java/com/smartnsoft/ws/okhttp/ReuseOkHttpClient.java
@@ -0,0 +1,19 @@
+package com.smartnsoft.ws.okhttp;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Ludovic Roland
+ * @since 2016.04.28
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Inherited
+public @interface ReuseOkHttpClient
+{
+
+}
