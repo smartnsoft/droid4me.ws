d32f0921e8a3c6a770dbd7d689443e5ea6d825ec
Ecalle Thomas <thomasecalle@hotmail.fr>
2018-12-11 15:28:26 +0100

    Put parameters on right order

--- retrofit/src/main/kotlin/com/smartnsoft/ws/retrofit/JacksonRetrofitWebServiceCaller.kt
+++ retrofit/src/main/kotlin/com/smartnsoft/ws/retrofit/JacksonRetrofitWebServiceCaller.kt
@@ -12,7 +12,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
  * @since 2018.03.26
  */
 abstract class JacksonRetrofitWebServiceCaller<API>(api: Class<API>, baseUrl: String, connectTimeout: Long = CONNECT_TIMEOUT, readTimeout: Long = READ_TIMEOUT, writeTimeout: Long = WRITE_TIMEOUT, cacheSize: Long = CACHE_SIZE, defaultCachePolicy: CachePolicy = CachePolicy.ONLY_NETWORK) :
-    RetrofitWebServiceCaller<API>(api, baseUrl, converterFactories = arrayOf(JacksonConverterFactory.create(), ScalarsConverterFactory.create()), connectTimeout = connectTimeout, readTimeout = readTimeout, writeTimeout = writeTimeout, cacheSize = cacheSize, defaultCachePolicy = defaultCachePolicy)
+    RetrofitWebServiceCaller<API>(api, baseUrl, connectTimeout, readTimeout, writeTimeout, cacheSize, defaultCachePolicy, arrayOf(JacksonConverterFactory.create(), ScalarsConverterFactory.create()))
 {
 
   private val mapper = ObjectMapper()
