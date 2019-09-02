5277b8e1427eadbb15132b6b38123682b0c93d70
Anthony Msihid <anthony@smartnsoft.com>
2019-09-02 10:49:56 +0200

    - Run tests with multiple parameters

--- retrofitsample/src/test/java/test/RDLVAuthenticator.kt
+++ retrofitsample/src/test/java/test/RDLVAuthenticator.kt
@@ -8,9 +8,12 @@ import com.smartnsoft.ws.retrofit.bo.AccessToken
 import com.smartnsoft.ws.retrofit.bo.ErrorResponse
 import com.smartnsoft.ws.retrofit.bo.ResponseWithError
 import com.smartnsoft.ws.retrofit.caller.AuthJacksonRetrofitWebServiceCaller
+import com.smartnsoft.ws.retrofit.caller.RetrofitWebServiceCaller
 import org.junit.FixMethodOrder
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.junit.runners.MethodSorters
+import org.junit.runners.Parameterized
 import java.io.File
 import java.lang.Exception
 
@@ -20,7 +23,8 @@ import java.lang.Exception
  * @since 2019.08.23
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-class RDLVAuthenticator
+@RunWith(Parameterized::class)
+class RDLVAuthenticator(private val shouldReturnErrorResponse: Boolean)
 {
   enum class RequestBehavior
   {
@@ -33,9 +37,16 @@ class RDLVAuthenticator
 
   companion object
   {
+    @JvmStatic
+    @Parameterized.Parameters(name = "shouldReturnErrorResponse = {0}")
+    fun data(): List<Array<Boolean>>
+    {
+      return mutableListOf(arrayOf(true), arrayOf(false))
+    }
+
     private var privateAccessToken: AccessToken? = null
 
-    private class AuthSimpleWebServiceCaller(val authProvider: AuthProvider, builtInCache: BuiltInCache? = BuiltInCache(shouldReturnErrorResponse = true), baseUrl: String) : AuthJacksonRetrofitWebServiceCaller<RDLVApi>(api = RDLVApi::class.java, baseUrl = baseUrl, builtInCache = builtInCache, authProvider = authProvider)
+    private class AuthSimpleWebServiceCaller(val authProvider: AuthProvider, builtInCache: BuiltInCache? = BuiltInCache(), baseUrl: String) : AuthJacksonRetrofitWebServiceCaller<RDLVApi>(api = RDLVApi::class.java, baseUrl = baseUrl, builtInCache = builtInCache, authProvider = authProvider)
     {
 
       init
@@ -62,7 +73,7 @@ class RDLVAuthenticator
     private lateinit var serviceCaller: AuthSimpleWebServiceCaller
 
     @JvmStatic
-    fun initializeService(requestBehavior: RequestBehavior)
+    fun initializeService(requestBehavior: RequestBehavior, shouldReturnErrorResponse: Boolean )
     {
       val authProvider = object : AuthProvider
       {
@@ -75,7 +86,7 @@ class RDLVAuthenticator
         {
           return when (requestBehavior)
           {
-            RequestBehavior.WRONG_XAPIKEY -> "wrongxapikey"
+            RequestBehavior.WRONG_XAPIKEY -> "wrong_xapikey"
             RequestBehavior.EMPTY_XAPIKEY -> ""
             else                          -> RDLVApi.xApiToken
           }
@@ -94,20 +105,20 @@ class RDLVAuthenticator
         }
       }
 
-      serviceCaller = AuthSimpleWebServiceCaller(baseUrl = authProvider.getBaseRoute(), authProvider = authProvider)
+      serviceCaller = AuthSimpleWebServiceCaller(baseUrl = authProvider.getBaseRoute(), authProvider = authProvider, builtInCache = RetrofitWebServiceCaller.BuiltInCache(shouldReturnErrorResponse = shouldReturnErrorResponse))
 
       when (requestBehavior)
       {
         RequestBehavior.WRONG_TOKEN             ->
         {
           authProvider.setAccessToken(privateAccessToken?.let { realAccessToken ->
-            AccessToken("wrongtoken", realAccessToken.refreshToken, realAccessToken.tokenType, realAccessToken.expiresIn)
+            AccessToken("wrong_token", realAccessToken.refreshToken, realAccessToken.tokenType, realAccessToken.expiresIn)
           })
         }
         RequestBehavior.WRONG_TOKEN_AND_REFRESH ->
         {
           authProvider.setAccessToken(privateAccessToken?.let { realAccessToken ->
-            AccessToken("wrongtoken", "wrongrefresh", realAccessToken.tokenType, realAccessToken.expiresIn)
+            AccessToken("wrong_token", "wrong_refresh", realAccessToken.tokenType, realAccessToken.expiresIn)
           })
         }
         else                                    ->
@@ -121,9 +132,10 @@ class RDLVAuthenticator
   @Test
   fun test_01_loginOk()
   {
-    initializeService(RequestBehavior.NORMAL)
-
+    initializeService(RequestBehavior.NORMAL, shouldReturnErrorResponse)
     serviceCaller.authProvider.setAccessToken(null)
+    serviceCaller.getCache()?.delete()
+
     val response = serviceCaller.login("j.dehais@equinoa.com", "api")
     val success = response?.successResponse != null
 
@@ -133,7 +145,7 @@ class RDLVAuthenticator
   @Test
   fun test_02_getInfoWithWrongApiKey()
   {
-    initializeService(RequestBehavior.WRONG_XAPIKEY)
+    initializeService(RequestBehavior.WRONG_XAPIKEY, shouldReturnErrorResponse)
 
     try
     {
@@ -158,7 +170,7 @@ class RDLVAuthenticator
   @Test
   fun test_03_relogAfter403()
   {
-    initializeService(RequestBehavior.NORMAL)
+    initializeService(RequestBehavior.NORMAL, shouldReturnErrorResponse)
 
     val response = serviceCaller.login("j.dehais@equinoa.com", "api")
     val success = response?.successResponse != null
@@ -169,7 +181,7 @@ class RDLVAuthenticator
   @Test
   fun test_04_404()
   {
-    initializeService(RequestBehavior.NORMAL)
+    initializeService(RequestBehavior.NORMAL, shouldReturnErrorResponse)
 
     try
     {
@@ -192,7 +204,7 @@ class RDLVAuthenticator
   @Test
   fun test_05_getInfoAfterLogin()
   {
-    initializeService(RequestBehavior.NORMAL)
+    initializeService(RequestBehavior.NORMAL, shouldReturnErrorResponse)
 
     // Waiting a bit
     Thread.sleep(1_000)
@@ -214,7 +226,7 @@ class RDLVAuthenticator
   @Test
   fun test_06_getInfoWithEmptyApiKey()
   {
-    initializeService(RequestBehavior.EMPTY_XAPIKEY)
+    initializeService(RequestBehavior.EMPTY_XAPIKEY, shouldReturnErrorResponse)
 
     try
     {
@@ -239,7 +251,7 @@ class RDLVAuthenticator
   @Test
   fun test_07_relogAfter403()
   {
-    initializeService(RequestBehavior.NORMAL)
+    initializeService(RequestBehavior.NORMAL, shouldReturnErrorResponse)
 
     val response = serviceCaller.login("j.dehais@equinoa.com", "api")
     val success = response?.successResponse != null
@@ -250,7 +262,7 @@ class RDLVAuthenticator
   @Test
   fun test_08_wrongAccessAndRefreshToken()
   {
-    initializeService(RequestBehavior.WRONG_TOKEN_AND_REFRESH)
+    initializeService(RequestBehavior.WRONG_TOKEN_AND_REFRESH, shouldReturnErrorResponse)
 
     try
     {
@@ -276,7 +288,7 @@ class RDLVAuthenticator
   @Test
   fun test_09_relogAfter403()
   {
-    initializeService(RequestBehavior.NORMAL)
+    initializeService(RequestBehavior.NORMAL, shouldReturnErrorResponse)
 
     val response = serviceCaller.login("j.dehais@equinoa.com", "api")
     val success = response?.successResponse != null
@@ -287,7 +299,7 @@ class RDLVAuthenticator
   @Test
   fun test_10_getInfoAfterRefresh()
   {
-    initializeService(RequestBehavior.WRONG_TOKEN)
+    initializeService(RequestBehavior.WRONG_TOKEN, shouldReturnErrorResponse)
     val oldToken = serviceCaller.authProvider.getAccessToken()
 
     try
@@ -308,7 +320,7 @@ class RDLVAuthenticator
   @Test
   fun test_11_loginKo()
   {
-    initializeService(RequestBehavior.NORMAL)
+    initializeService(RequestBehavior.NORMAL, shouldReturnErrorResponse)
 
     serviceCaller.authProvider.setAccessToken(null)
     val response = serviceCaller.login("wrong@equinoa.com", "wrong")
